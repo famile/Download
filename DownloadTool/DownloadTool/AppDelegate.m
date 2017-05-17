734bd4452ef86414ff57fd50f8e8e110fc7647e0
litao <437534725@qq.com>
2017-05-17 14:44:17 +0800

    添加延迟杀死

--- DownloadTool/DownloadTool/AppDelegate.m
+++ DownloadTool/DownloadTool/AppDelegate.m
@@ -11,6 +11,8 @@
 
 @interface AppDelegate ()
 
+@property(assign, nonatomic)UIBackgroundTaskIdentifier backIden;
+
 @end
 
 @implementation AppDelegate
@@ -36,8 +38,19 @@
 
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    
+    NSLog(@"begin=============");
+    _backIden = [[UIApplication sharedApplication] beginBackgroundTaskWithExpirationHandler:^{
+        //在时间到之前会进入这个block，一般是iOS7及以上是3分钟。按照规范，在这里要手动结束后台，你不写也是会结束的（据说会crash）
+        NSLog(@"将要挂起=============");
+        [self endBack];
+    }];
+}
+-(void)endBack
+{
+    NSLog(@"end=============");
+    [[UIApplication sharedApplication] endBackgroundTask:_backIden];
+    _backIden = UIBackgroundTaskInvalid;
 }
 
 
